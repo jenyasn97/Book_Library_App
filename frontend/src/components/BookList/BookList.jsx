70592e7a908cdf21f3546852cd6bdb591560a092
John Sultan <jenya.sultan@yandex.ru>
2024-09-19 13:13:15 +0300

    initial Redux and add display books

--- frontend/src/components/BookList/BookList.jsx
+++ frontend/src/components/BookList/BookList.jsx
@@ -1,8 +1,26 @@
 import "./BookList.css";
+import { useSelector } from "react-redux";
 function BookList() {
+  const books = useSelector((state) => state.books);
   return (
     <div className="app-block book-list">
       <h2>Book List </h2>
+      {books.length === 0 ? (
+        <p> No Books available</p>
+      ) : (
+        <ul>
+          {books.map((book, idx) => {
+            return (
+              <li key={idx}>
+                <div className="book-info">
+                  {`${++idx}. ${book.title} by `}
+                  {<strong>{book.author}</strong>}
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 }
