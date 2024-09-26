34da839e57471a8cfc2ceee94968153c8a97a60d
John Sultan <jenya.sultan@yandex.ru>
2024-09-26 15:15:00 +0300

    added errorSlice

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import filterReduser from "./slices/filterSlice";
 import booksReducer from "./slices/booksSlice";
+import errorReducer from "./slices/errorSlice";
 const store = configureStore({
   reducer: {
     books: booksReducer,
     filter: filterReduser,
+    error: errorReducer,
   },
 });
 
